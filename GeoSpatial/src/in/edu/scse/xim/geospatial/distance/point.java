8118113a83e769cfc4e2b2761cdcd370d5a7ca37
sambitashibarpitamohanty <72293310+sambitashibarpitamohanty@users.noreply.github.com>
2021-12-09 19:52:26 +0530

    Update point.java

--- GeoSpatial/src/in/edu/scse/xim/geospatial/distance/point.java
+++ GeoSpatial/src/in/edu/scse/xim/geospatial/distance/point.java
@@ -3,17 +3,17 @@ package in.edu.scse.xim.geospatial.distance;
 public class point {
     public static double findDistance(double x, double y, double a, double b) {
         double L1 = x / 57.29577951; // converting the given values to radians
-        double Y1 = y / 57.29577951;
-        double L2 = a / 57.29577951;
-        double Y2 = b / 57.29577951;
+        double Y1 = y / 57.29577951; // converting the given values to radians
+        double L2 = a / 57.29577951; // converting the given values to radians
+        double Y2 = b / 57.29577951; // converting the given values to radians
         System.out.println("The given latitude and longitude are: " + L1 + " " + Y1);
         System.out.println("The given latitude and longitude are: " + L2 + " " + Y2);
-        double Latitude = L2 - L1;
+        double Latitude = L2 - L1; //haversine formula
         double Longitude = Y2 - Y1;
         double D = Math.pow(Math.sin(Latitude / 2), 2)
                 + Math.cos(L1) * Math.cos(L2) * Math.pow(Math.sin(Longitude / 2), 2);
         double c = 2 * Math.asin(Math.sqrt(D));
-        double r = 6371;
-        return (c * r);
+        double r = 6371; //radius of the earth in kilometers
+        return (c * r); // calculating the result
     }
 }
